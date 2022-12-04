27459e66887ed5697fead44af908f158f91787ca
Adam <adamlederer@live.ca>
2022-12-03 22:34:41 -0700

    started work. Got object to display in separate lines

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TweetPosts/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TweetPosts from './components/TweetPosts.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TweetPosts
   }
 }
 </script>
---
+++ src/components/TweetPosts.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button>Tweet</button>
+        <article v-for="tweet in tweets" :key="tweet[`post`]">
+            <p>{{tweet[`posts`]}}</p>
+            <p>{{tweet[`user`]}}</p>
+            <p>{{tweet[`date`]}}</p>
+        </article>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TweetPosts",
+        data(){
+            return{
+                tweets :[
+                    {
+                    posts: "Did you mean Blake?",
+                    user: "Blake",
+                    date: "03/12/2022",
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
