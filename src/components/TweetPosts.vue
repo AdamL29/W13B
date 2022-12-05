5995fe1b351a8e479e970a1abab54612a11ac1a1
Adam L <adamlederer@live.ca>
2022-12-04 22:28:31 -0700

    Attempted to read array. Couldn't figure out cowhy my button wouldn't work for the changes and reverted it inital button use.

--- src/components/TweetPosts.vue
+++ src/components/TweetPosts.vue
@@ -1,10 +1,11 @@
+
 <template>
     <div>
-        <button>Tweet</button>
-        <article v-for="tweet in tweets" :key="tweet[`post`]">
-            <p>{{tweet[`posts`]}}</p>
-            <p>{{tweet[`user`]}}</p>
-            <p>{{tweet[`date`]}}</p>
+        <button>Switch</button>
+        <article v-for="(tweet, index) in tweets" :key="index">
+            <p>{{tweets[`posts`]}}</p>
+            <p>{{tweets[`users`]}}</p>
+            <p>{{tweets[`dates`]}}</p>
         </article>
     </div>
 </template>
@@ -14,18 +15,49 @@
         name: "TweetPosts",
         data(){
             return{
-                tweets :[
-                    {
-                    posts: "Did you mean Blake?",
-                    user: "Blake",
-                    date: "03/12/2022",
+                isList: true,
+                tweets:{
+                    posts: ["Did you mean Blake?", "It's Jacqueline"],
+                    users: ["Blake", "Jacqueline"],
+                    dates: ["03/12/2022", "05/09/2021"],
                     }
-                ]
+                
             }
-        }
+        },
+        methods: {
+                gridList(){
+                    if (this.isList){
+                        document.body.style.backgroundColor = `white`;
+                    }else{
+                        document.body.style.backgroundColor = `blue`;
+                    }
+                    this.isList = !this.isList
+                    console.log(`it worked`);
+                },
+                // showPost() {
+                //     let myDiv = document.querySelector(`article`);
+                //     for (let tweet of this.tweets.posts){
+                //     myDiv.insertAdjacentHTML(`beforeend`, `<p>${tweet}</p>`)
+                //     }
+                //     for (let user of this.tweets.users){
+                //     myDiv.insertAdjacentHTML(`beforeend`, `<p>${user}</p>`)
+                //     }
+                //     for (let date of this.tweets.dates){
+                //     myDiv.insertAdjacentHTML(`beforeend`, `<p>${date}</p>`)
+                //     }
+                // }
+            },
+            mounted () {
+                document.querySelector(`button`).addEventListener(`click`, this.gridList);
+                // this.showPost();
+            },
     }
 </script>
 
 <style scoped>
-
+    article{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        border: navy 2px solid;
+    }
 </style>
